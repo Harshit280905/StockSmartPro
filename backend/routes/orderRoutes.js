f8477fbbfd6cb8095a27b8121c9c2c41257aba1d
Ojasv03 <156661653+Ojasv03@users.noreply.github.com>
2026-05-12 22:15:15 +0530

    implement order routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { authMiddleware } = require('../middlewares/authMiddleware');
+const { createOrder, getOrders } = require('../controllers/orderController');
+
+router.post('/', authMiddleware, createOrder);
+router.get('/', authMiddleware, getOrders);
+
+module.exports = router;
